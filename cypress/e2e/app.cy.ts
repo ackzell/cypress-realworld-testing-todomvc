576eeb55a9eb2bbf493fb80f2c77c4f3217422b7
ackzell <ackzell@gmail.com>
2022-06-23 20:08:23 -0500

    Creating constants

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,15 +1,28 @@
 /// <reference types="cypress" />
 
 describe("React TodoMVC", () => {
-  it("adds a single todo", () => {
-    cy.visit("http://localhost:8888")
+  const TODO_ITEM_ONE = "Buy Milk"
+  const TODO_ITEM_TWO = "Pay Rent"
+  const TODO_ITEM_THREE = "Pickup Dry Cleaning"
 
+  beforeEach(() => {
+    cy.visit("http://localhost:8888")
+  })
+
+  it("adds a single todo", () => {
     //* You can chain the `type()` calls and prevent `get()`ting the same element more than once
-    cy.get(".new-todo")
-      .type("Buy rootbeer{enter}")
-      .type("Keep rootbeer in the fridge{enter}")
+    cy.get(".new-todo").type(`${TODO_ITEM_ONE}{enter}`)
 
     //* assertion
-    cy.get(".todo-list li").should("have.length", 2)
+    cy.get(".todo-list li").should("have.length", 1)
+  })
+
+  it("adds three todos", () => {
+    cy.get(".new-todo")
+      .type(`${TODO_ITEM_ONE}{enter}`)
+      .type(`${TODO_ITEM_TWO}{enter}`)
+      .type(`${TODO_ITEM_THREE}{enter}`)
+
+    cy.get(".todo-list li").should("have.length", 3)
   })
 })
